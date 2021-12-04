ba4c00ec917130045302fe61b21379b3ff115d92
Joxit <joxit972@gmail.com>
2021-12-04 21:29:42 +0100

    feat(audio format): add some function and more informations (bitrate/compression factor)

--- app/src/main/java/dev/joxit/androidapp/audiorecorder/entity/AudioFormatEntity.kt
+++ app/src/main/java/dev/joxit/androidapp/audiorecorder/entity/AudioFormatEntity.kt
@@ -1,25 +1,33 @@
 package dev.joxit.androidapp.audiorecorder.entity
 
 import dev.joxit.androidapp.audiorecorder.R
+import kotlin.math.ceil
+import kotlin.math.floor
 
 enum class AudioFormatEntity(
   override val iconId: Int,
   override val shortTitleId: Int,
   override val titleId: Int,
-  override val descriptionId: Int
+  override val descriptionId: Int,
+  val bitRate: Int,
+  val compressionFactor: Double
 ) : IconTitleDescription {
   WAV(
     R.drawable.icon_superior,
     R.string.AURE_WAV_QUALITY_TITLE_SUPERIOR,
     R.string.AURE_DETAILS_BAR_QUALITY_SUPERIOR,
-    R.string.AURE_WAV_QUALITY_SUBTITLE_SUPERIOR
+    R.string.AURE_WAV_QUALITY_SUBTITLE_SUPERIOR,
+    705600,
+    1.0,
   ),
 
   AAC128(
     R.drawable.icon_high,
     R.string.AURE_AAC128_QUALITY_TITLE_HIGH,
     R.string.AURE_DETAILS_BAR_QUALITY_HIGH,
-    R.string.AURE_AAC128_QUALITY_SUBTITLE_HIGH
+    R.string.AURE_AAC128_QUALITY_SUBTITLE_HIGH,
+    128000,
+    1.0191,
   ),
 
   AAC64(
@@ -27,12 +35,35 @@ enum class AudioFormatEntity(
     R.string.AURE_AAC64_QUALITY_TITLE_MEDIUM,
     R.string.AURE_DETAILS_BAR_QUALITY_MEDIUM,
     R.string.AURE_AAC64_QUALITY_SUBTITLE_MEDIUM,
+    64000,
+    1.04767,
   ),
   AAC32(
     R.drawable.icon_basic,
     R.string.AURE_AAC32_QUALITY_TITLE_BASIC,
     R.string.AURE_DETAILS_BAR_QUALITY_BASIC,
-    R.string.AURE_AAC32_QUALITY_SUBTITLE_BASIC
-  ),
+    R.string.AURE_AAC32_QUALITY_SUBTITLE_BASIC,
+    32000,
+    1.0855,
+  );
 
+  fun getContainer() = if (this == WAV) "wav" else "aac"
+
+  fun getFormat() = if (this == WAV) "WAV" else "AAC LC"
+
+  fun getMimeType() = if (this == WAV) "audio/raw" else "audio/m4a-latm"
+
+  fun getSampleRate(): Int = 44100
+
+  fun getBitsPerSample(): Short = 16
+
+  fun getMaxFileSize(): Int = if (this == WAV) 2146435071 else -1
+
+  fun getBufferSize() = 0
+
+  fun getSampleSize(duration: Double, channels: Short): Long =
+    ceil(duration * ((bitRate * channels) / 8) * compressionFactor).toLong()
+
+  fun getEstimatedTime(s: Short, j: Long): Long =
+    floor((j / getSampleSize(1.0, s)).toDouble()).toLong()
 }
