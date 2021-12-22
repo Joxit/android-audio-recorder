963ae1033300c05e7194be6c4b4a0f9015478305
Joxit <joxit972@gmail.com>
2021-12-22 22:02:41 +0100

    feat(recorder service): implement the service using the controller

--- app/src/main/java/dev/joxit/androidapp/audiorecorder/activity/service/recorder/AudioRecorderService.kt
+++ app/src/main/java/dev/joxit/androidapp/audiorecorder/activity/service/recorder/AudioRecorderService.kt
@@ -6,8 +6,12 @@ import android.os.Binder
 import android.os.IBinder
 import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import dev.joxit.androidapp.audiorecorder.activity.service.recorder.manager.AudioHandlerListener
+import dev.joxit.androidapp.audiorecorder.activity.service.recorder.manager.RecorderManager
+import dev.joxit.androidapp.audiorecorder.model.Entry
 
-class AudioRecorderService : IntentService("AudioRecorderService") {
+class AudioRecorderService : IntentService("AudioRecorderService"),
+  AudioRecorderController.RecorderStateListener {
   private var broadcastManager: LocalBroadcastManager? = null
   private var isDestroyed = true
   private var controller: AudioRecorderController? = null
@@ -26,6 +30,7 @@ class AudioRecorderService : IntentService("AudioRecorderService") {
     super.onCreate()
     broadcastManager = LocalBroadcastManager.getInstance(applicationContext)
     isDestroyed = false
+    controller = AudioRecorderController(this, this)
     sendBroadcast(SERVICE_CREATED_INTENT)
   }
 
@@ -33,16 +38,80 @@ class AudioRecorderService : IntentService("AudioRecorderService") {
     super.onDestroy()
     isDestroyed = true
     broadcastManager = null
+    controller?.onDestroy()
+    controller = null
   }
 
   override fun onBind(intent: Intent?): IBinder = binder
 
   private fun getElapsedTime(): Int {
-    return this.controller?.elapsedTime ?: 0
+    return controller?.elapsedTime ?: 0
   }
 
   private fun getVolume(): DoubleArray? {
-    return this.controller?.volume
+    return controller?.volume
+  }
+
+  override fun onTaskRemoved(intent: Intent?) {
+    super.onTaskRemoved(intent)
+    System.currentTimeMillis()
+    controller?.onTaskRemoved()
+  }
+
+  @Synchronized
+  fun stop() {
+    controller?.let {
+      if (!isDestroyed) {
+        it.stop()
+      } else {
+        it.onDestroy()
+      }
+    }
+  }
+
+  @Synchronized
+  fun start(entry: Entry, recorderManager: RecorderManager) {
+    controller?.let {
+      if (!isDestroyed) {
+        it.start(entry, recorderManager)
+      } else {
+        it.stop()
+      }
+    }
+  }
+
+  @Synchronized
+  fun pause() {
+    controller?.let {
+      if (isDestroyed) {
+        it.pause()
+      } else {
+        it.stop()
+      }
+    }
+  }
+
+  @Synchronized
+  fun resume() {
+    controller?.let {
+      if (isDestroyed) {
+        it.resume()
+      } else {
+        it.stop()
+      }
+    }
+  }
+  override fun onRecorderIdle() {
+    gotoIdleState()
+  }
+
+  private fun gotoIdleState() {}
+
+  override fun onRecorderReleased() {
+    if (!this.isDestroyed) {
+      gotoIdleState()
+      stopSelf()
+    }
   }
 
   inner class AudioRecorderBinder : Binder() {
@@ -50,7 +119,7 @@ class AudioRecorderService : IntentService("AudioRecorderService") {
       get() = this@AudioRecorderService.getVolume()
     val elapsedTime: Int
       get() = this@AudioRecorderService.getElapsedTime()
-//    val audioHandlerListener: AudioHandlerListener
-//      get() = this@AudioRecorderService.controller
+    val audioHandlerListener: AudioHandlerListener
+      get() = this@AudioRecorderService.controller!!
   }
 }
